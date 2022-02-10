be262a40dde834d2fba5237a09cb110c9b508cb8
Tomas Dinis <dev.tomas2003@gmail.com>
2022-02-10 01:13:52 +0000

    Envio dos enimigos

--- src/server.js
+++ src/server.js
@@ -17,14 +17,42 @@ const io = socket(server, {
 
 const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 
-io.on('connection', function(socket){
+var rooms = [];
+
+io.on('connection', function (socket) {
     socket.once("create", (props) => {
+        const gamerInfo = JSON.parse(props);
         const partyInfo = {
             "code": genRanHex(5).toUpperCase()
         }
+        rooms = [...rooms, {
+            code: partyInfo.code,
+            player: [{
+                nome: gamerInfo.playerName,
+                socket: socket
+            }
+            ]
+        }]
         socket.emit("partyInfo", JSON.stringify(partyInfo));
     });
     socket.once("join", (props) => {
-        console.log(props);
+        const partyInfo = JSON.parse(props);
+        if (rooms.findIndex(x => x.code === partyInfo.code) === -1) {
+            socket.emit("partyError", 0);
+        } else {
+            const posicao = rooms.findIndex(x => x.code === partyInfo.code);
+            const partyData = {
+                enemy: rooms[posicao].player[0].nome
+            }
+            rooms[posicao].player = [...rooms[posicao].player, {
+                nome: partyInfo.playerName,
+                socket: socket
+            }];
+            const enemyData = {
+                enemy: partyInfo.playerName
+            }
+            socket.emit("partyData", JSON.stringify(partyData));
+            rooms[posicao].player[0].socket.emit("enemyJoin", JSON.stringify(enemyData));
+        }
     });
 })
